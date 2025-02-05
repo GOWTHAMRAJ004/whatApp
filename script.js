375f0c921ed927abf27242e32df89ea83b5fab6b
Gowthamraj S <rgowtham@CEIIND.COM>
2025-02-05 18:08:26 +0530

    Completed all the functions

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ document.querySelectorAll('.profiles').forEach(profile => {
         let selectedImage = this.getAttribute('data-image');
         let selectedMessage = this.getAttribute('data-message');
 
-        document.getElementById('chat-user-name').textContent = selectedName;
-        document.getElementById('chat-user-image').src = selectedImage;
-        document.getElementById('chat-user-message').textContent = selectedMessage;
+        document.getElementById('chatUserName').textContent = selectedName;
+        document.getElementById('chatUserImage').src = selectedImage;
+        document.getElementById('chatUserMessage').textContent = selectedMessage;
+        document.querySelector(".chartBody").innerHTML = "";
     });
 });
 document.querySelector(".charts").addEventListener("keypress", function(event) {
@@ -17,15 +18,22 @@ document.querySelector(".charts").addEventListener("keypress", function(event) {
         
         let inputField = event.target;
         let message = inputField.value.trim();
-        
+        let now = new Date();
+        let hours = now.getHours().toString().padStart(2, '0');
+        let minutes = now.getMinutes().toString().padStart(2, '0');
+        let currentTime = `${hours}:${minutes}`;
+        console.log(currentTime);
         if (message !== "") {
             let chatBody = document.querySelector(".chartBody");
             let messageElement = document.createElement("div");
-            
+            let time = document.createElement("H6");
+            time.classList.add("timer");
+            time.innerText  = currentTime;  
             messageElement.classList.add("chatMessage");
             messageElement.innerText = message;
-
+            messageElement.appendChild(time);
             chatBody.appendChild(messageElement);
+        
             chatBody.scrollTop = chatBody.scrollHeight;
             
             inputField.value = ""; 
